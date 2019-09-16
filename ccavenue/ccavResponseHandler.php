dac24dd268b01aa7ebc386443309757ac314a4ed
Maran <maran.happysanz@gmail.com>
2019-09-16 16:13:35 +0530

    Update ccavResponseHandler.php

--- ccavenue/ccavResponseHandler.php
+++ ccavenue/ccavResponseHandler.php
@@ -78,8 +78,6 @@ for($i = 0; $i < $dataSize; $i++)
         $user_id= $result[1];
         $purchase_id = $result[2]; */
 		
-        echo $order_status;
-
         //echo $sQuery = "INSERT INTO ccavenue_status (order_id,user_id,track_id,bank_ref_no,order_status,failure_message,payment_mode,card_name,status_code,status_message,currency,amount,billing_name,billing_address, billing_city,billing_state,billing_zip,billing_country,billing_tel,billing_email,delievery_name,delievery_address,delievery_city,delievery_state,delievery_zip,delievery_country,delievery_tel,merch_param1,merch_param2,merch_param3,merch_param4,merch_param5,vault,offer_type,offer_code,discount_value, mer_amt,eci_value,retry,response_code,billing_notes,trans_date,bin_country) VALUES ('$orderid','$cust_id','$track_id','$bank_ref_no','$order_status','$failure_message','$payment_mode','$card_name','$status_code','$status_message','$currency','$amount','$billing_name','$billing_address','$billing_city','$billing_state','$billing_zip','$billing_country','$billing_tel','$billing_email','$delievery_name','$delievery_address','$delievery_city','$delievery_state','$delievery_zip','$delievery_country','$delievery_tel','$merch_param1','$merch_param2','$merch_param3','$merch_param4','$merch_param5','$vault','$offer_type','$offer_code','$discount_value','$mer_amt','$eci_value','$retry','$response_code','$billing_notes','$transdate','$bin_country')";
         //$objRs  = mysql_query($sQuery) or die("Could not select Query ");
 
@@ -87,25 +85,25 @@ for($i = 0; $i < $dataSize; $i++)
 
     	if($order_status=="Success")
     	{
-			echo $query = "UPDATE purchase_order SET status = 'Success' WHERE order_id = '$orderid'";
+			$query = "UPDATE purchase_order SET status = 'Success' WHERE order_id = '$orderid'";
 			$objRs  = mysql_query($query) or die("Could not select Query ");
 			
-			 //header("Location: https://www.happysanztech.com/lamore/");
+			 header("Location: https://www.happysanztech.com/lamore/");
     	}
 
     	if($order_status=="Aborted")
     	{
-    	   //header("Location: https://www.happysanztech.com/lamore/");
+    	   header("Location: https://www.happysanztech.com/lamore/");
     	}
     	
     	if($order_status=="Failure")
     	{
-    	   // header("Location: https://www.happysanztech.com/lamore/");
+    	    header("Location: https://www.happysanztech.com/lamore/");
     	}
     	
     	if($order_status=="Invalid")
     	{
-    	    //header("Location: https://www.happysanztech.com/lamore/");
+    	    header("Location: https://www.happysanztech.com/lamore/");
 
     	}
 
