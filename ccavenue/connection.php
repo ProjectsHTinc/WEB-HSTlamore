c7b37f23e645afdd737ee12be27c16d151579d9f
Maran <maran.happysanz@gmail.com>
2019-09-17 18:10:29 +0530

    Update connection.php

--- ccavenue/connection.php
+++ ccavenue/connection.php
@@ -3,7 +3,7 @@
 //$con = @mysql_connect("localhost","root","");
 $con = @mysql_connect("localhost","root","O+E7vVgBr#{}");
 if ($con) {
-		mysql_select_db('happynak_lamore');
+		mysql_select_db('happysan_lamore');
     } else {
 		die("Connection failed");
 }
