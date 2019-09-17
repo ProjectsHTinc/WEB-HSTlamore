5248f1e7c6a1054168db5925e565c80f4ae29686
Maran <maran.happysanz@gmail.com>
2019-09-17 17:43:46 +0530

    Update ccavRequestHandler.php

--- ccavenue/ccavRequestHandler.php
+++ ccavenue/ccavRequestHandler.php
@@ -19,42 +19,45 @@ include("connection.php");
 	$access_code='AVAU84GD83BV10UAVB';//Shared by CCAVENUES
 	
 	
-	foreach ($_POST as $key => $value){
+	/* foreach ($_POST as $key => $value){
 		$merchant_data.=$key.'='.$value.'&';
-	}
+	} */
     
 
-exit;
 
-    $merchant_id = $_POST["merchant_id"];
-	$order_id = $_POST["order_id"];
-	$amount = trim($_POST["amount"]);
-	$currency = $_POST["currency"];
-	$redirect_url = $_POST["redirect_url"];
-	$cancel_url = $_POST["cancel_url"];
-	$language = $_POST["language"];
+
+    echo $merchant_id = $_POST["merchant_id"];
+	echo $order_id = $_POST["order_id"];
+	echo $amount = trim($_POST["amount"]);
+	echo $currency = $_POST["currency"];
+	echo $redirect_url = $_POST["redirect_url"];
+	echo $cancel_url = $_POST["cancel_url"];
+	echo $language = $_POST["language"];
 
 	$resp_data = "merchant_id=".$merchant_id."&";
 	$resp_data .= "order_id=".$order_id."&";
 	//$resp_data .= "amount=".$amount."&";
 	
- 	$sQuery = "SELECT total_amount FROM purchase_order WHERE order_id = '$order_id' LIMIT 1";
+ 	echo $sQuery = "SELECT total_amount FROM purchase_order WHERE order_id = '$order_id' LIMIT 1";
 	$objRs = mysql_query($sQuery);
 	while ($row = mysql_fetch_array($objRs))
 	{
-		$purchase_amount  = trim($row['total_amount']);
+		echo $purchase_amount  = trim($row['total_amount']);
 	}
 
 	if ($amount != $purchase_amount){
-		$resp_data .= "amount=".$purchase_amount."&";
+		echo $resp_data .= "amount=".$purchase_amount."&";
 	} else {
-		$resp_data .= "amount=".$amount."&";
+		echo $resp_data .= "amount=".$amount."&";
 	} 
 	
 	$resp_data .= "currency=".$currency."&";
 	$resp_data .= "redirect_url=".$redirect_url."&";
 	$resp_data .= "cancel_url=".$cancel_url."&";
 	$resp_data .= "language=".$language."&";
+	
+	echo $resp_data;
+	exit;
 	
 
 	$encrypted_data=encrypt($resp_data,$working_key); // Method for encrypting the data.
